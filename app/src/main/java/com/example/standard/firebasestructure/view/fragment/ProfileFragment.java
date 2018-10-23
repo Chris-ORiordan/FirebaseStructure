115ad1dd562fbd74dd14ff26a2e69fce9a0e000b
Chris <Chris.P.O'Riordan@aib.ie>
2018-10-23 16:55:55 +0100

    add recycler for dest to profile and related

--- app/src/main/java/com/example/standard/firebasestructure/view/fragment/ProfileFragment.java
+++ app/src/main/java/com/example/standard/firebasestructure/view/fragment/ProfileFragment.java
@@ -6,6 +6,7 @@ import android.arch.lifecycle.Observer;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.*;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,9 +14,9 @@ import android.widget.*;
 
 import com.example.standard.firebasestructure.*;
 import com.example.standard.firebasestructure.R;
-import com.example.standard.firebasestructure.model.User;
-import com.example.standard.firebasestructure.model.adapters.UserAdapter;
-import com.example.standard.firebasestructure.view.MainActivity;
+import com.example.standard.firebasestructure.model.*;
+import com.example.standard.firebasestructure.model.adapters.*;
+import com.example.standard.firebasestructure.view.*;
 import com.example.standard.firebasestructure.viewmodel.*;
 import com.squareup.haha.perflib.Main;
 
@@ -24,11 +25,13 @@ import java.util.*;
 public class ProfileFragment extends Fragment {
 
     private EditText editTextNewName;
-    private TextView textViewNumDestinations;
-    private TextView textViewNumFriends;
+    private RecyclerView recyclerViewDestinations;
+    private RecyclerView.Adapter recyclerAdapter;
+    private RecyclerView.LayoutManager recyclerLayoutManager;
     private Button buttonUpdate;
 
     private UserViewModel userViewModel;
+    private VenueViewModel venueViewModel;
     private OutGoerViewModel outGoerViewModel;
 
     public ProfileFragment() {
@@ -52,16 +55,44 @@ public class ProfileFragment extends Fragment {
         ((MainActivity) getActivity()).setDisplayHomeAsUpEnabled(false);
 
         editTextNewName = fragmentView.findViewById(R.id.editTextNewName);
-        textViewNumDestinations = fragmentView.findViewById(R.id.textViewNumDestinations);
-        textViewNumFriends = fragmentView.findViewById(R.id.textViewNumFriends);
+        recyclerViewDestinations = fragmentView.findViewById(R.id.recyclerDestinations);
+        recyclerViewDestinations.setHasFixedSize(true);
         buttonUpdate = fragmentView.findViewById(R.id.buttonUpdate);
 
         userViewModel = ViewModelProviders.of(this).get(UserViewModel.class);
+        venueViewModel = ViewModelProviders.of(this).get(VenueViewModel.class);
         outGoerViewModel = ViewModelProviders.of(this).get(OutGoerViewModel.class);
 
         editTextNewName.setText(MainApplication.getCurrentUser().getUserName());
-        textViewNumDestinations.setText(MainApplication.getCurrentUser().getDestinations().size() + " Destinations");
-        textViewNumFriends.setText(MainApplication.getCurrentUser().getFriends().size() + " Friends");
+
+        if(venueViewModel != null){
+            LiveData<List<Venue>>destinationVenues = venueViewModel.getDestinationVenueLiveData();
+
+            destinationVenues.observe(this, new Observer<List<Venue>>() {
+                @Override
+                public void onChanged(@Nullable List<Venue> destinations) {
+                    recyclerLayoutManager = new LinearLayoutManager(getContext());
+                    recyclerViewDestinations.setLayoutManager(recyclerLayoutManager);
+                    recyclerAdapter = new ProfileDestinationRecyclerAdapter(getContext(), destinations, new OnItemClickListener() {
+                        @Override
+                        public void onItemClick(Venue venue) {
+
+                        }
+
+                        @Override
+                        public void onItemClick(User friend) {
+
+                        }
+
+                        @Override
+                        public void onItemClick(OutGoer outGoer) {
+
+                        }
+                    });
+                    recyclerViewDestinations.setAdapter(recyclerAdapter);
+                }
+            });
+        }
 
         buttonUpdate.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/standard/firebasestructure/viewmodel/VenueViewModel.java
+++ app/src/main/java/com/example/standard/firebasestructure/viewmodel/VenueViewModel.java
@@ -4,6 +4,7 @@ import android.arch.core.util.Function;
 import android.arch.lifecycle.*;
 import android.support.annotation.NonNull;
 
+import com.example.standard.firebasestructure.MainApplication;
 import com.example.standard.firebasestructure.model.*;
 import com.example.standard.firebasestructure.network.FirebaseQueryLiveData;
 import com.google.firebase.database.*;
@@ -14,12 +15,20 @@ public class VenueViewModel extends ViewModel {
     private static final DatabaseReference VENUE_REF = FirebaseDatabase.getInstance().getReference().child("Venues");
 
     private List<Venue> venueList = new ArrayList<>();
+    private List<Venue> destinationList = new ArrayList<>();
 
     @NonNull
     public LiveData<List<Venue>> getVenueLiveData(){
         FirebaseQueryLiveData liveData = new FirebaseQueryLiveData(VENUE_REF);
         LiveData<List<Venue>> venuesLiveData = Transformations.map(liveData, new Deserialiser());
         return venuesLiveData;
+    }
+
+    @NonNull
+    public LiveData<List<Venue>> getDestinationVenueLiveData(){
+        FirebaseQueryLiveData liveData = new FirebaseQueryLiveData(VENUE_REF);
+        LiveData<List<Venue>> destinationLiveData = Transformations.map(liveData, new DestinationDeserialiser());
+        return destinationLiveData;
     }
 
     private class Deserialiser implements Function<DataSnapshot, List<Venue>> {
@@ -31,6 +40,25 @@ public class VenueViewModel extends ViewModel {
                 venueList.add(venue);
             }
             return venueList;
+        }
+    }
+
+    private class DestinationDeserialiser implements Function<DataSnapshot, List<Venue>>{
+        @Override
+        public List<Venue> apply(DataSnapshot dataSnapshot) {
+            destinationList.clear();
+            for(DataSnapshot dsp: dataSnapshot.getChildren()){
+                Venue destination = dsp.getValue(Venue.class);
+                if(destination.getAttendees() != null && MainApplication.getCurrentUser().getDestinations() != null){
+                    for(String venueId: MainApplication.getCurrentUser().getDestinations().keySet()){
+                        if(venueId.equals(destination.getVenueId())){
+                            destinationList.add(destination);
+                        }
+                    }
+                }
+
+            }
+            return destinationList;
         }
     }
 
@@ -49,4 +77,6 @@ public class VenueViewModel extends ViewModel {
     public void updateVenue(Venue currentVenue, Map<String, Object> newNameMap){
         VENUE_REF.child(currentVenue.getVenueId()).updateChildren(newNameMap);
     }
+
+
 }
